f03f5c7dcc5cb021caf8d23fd3c48960f5b759fa
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2024-03-22 00:34:29 +0500

    added login

--- routes/auth.js
+++ routes/auth.js
@@ -19,10 +19,7 @@ router.post('/signup', [
     });
   }
 
-  // Extract email and password from request body
   const { email } = req.body;
-
-  // Check if the email and password match any admin
   const adminAlreadyExist = data.admins.some(admin => admin.email === email);
 
   if (adminAlreadyExist) {
@@ -46,8 +43,44 @@ router.post('/signup', [
   });
 })
 
-router.post('/login' , (req, res) => {
-  res.send("I am Login")
+router.post('/login' , [
+  body('email').isEmail().withMessage('Invalid email address.'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long.')
+], (req, res) => {
+  // Check for validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      message: errors.array()[0].msg,
+      error: true,
+      statusCode: 400,
+      data: null
+    });
+  }
+
+  const { email, password } = req.body;
+  const userExists = data.admins.find(admin => admin.email === email && admin.password === password);
+
+  if (!userExists) {
+    // If user not found, return error message
+    return res.status(401).json({
+      message: 'Invalid email or password',
+      error: true,
+      statusCode: 401,
+      data: null
+    });
+  }
+
+  // If user found, return success message
+  return res.json({
+    message: 'Your are logged in successfully.',
+    error: false,
+    statusCode: 200,
+    data: {
+      token: 'abcd',
+      user: userExists
+    }
+  });
 })
 
 module.exports = router
